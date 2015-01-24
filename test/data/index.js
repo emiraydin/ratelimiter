8dcd1d2a47e788ae306cc4211d59dda4385ec733
Emir Aydin <emir@emiraydin.com>
2015-01-24 13:43:09 -0500

    Update test data

--- test/data/index.js
+++ test/data/index.js
@@ -7,10 +7,10 @@ var l1 = new Limit("{sourceName}:global:daily", 86400, 4000),
 	l4 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
 
 // Second limit set for expire tests
-var l5 = new Limit("{sourceName}:global:daily", 86400, 4000),
-	l6 = new Limit("{sourceName}:global:hourly", 3600, 2000),
-	l7 = new Limit("{sourceName}:{userID}:daily", 86400, 500),
-	l8 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
+var l5 = new Limit("{sourceName}:global:5second", 5, 4000),
+	l6 = new Limit("{sourceName}:global:3second", 3, 2000),
+	l7 = new Limit("{sourceName}:{userID}:3second", 3, 500),
+	l8 = new Limit("{sourceName}:{userID}:1second", 1, 100);
 
 var limits = [[l1,l2,l3,l4],[l5,l6,l7,l8]];
 
