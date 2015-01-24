e22f74c0351322c6a3036f7d62b7eac50e6baad7
Emir Aydin <emir@emiraydin.com>
2015-01-23 19:10:46 -0500

    Add callbacks for allow and block functions with a test call

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ var USER_HOURLY_LIMIT = 3;
 
 var RateLimiter = require('./ratelimiter');
 
+var rl = new RateLimiter("fitbit", GLOBAL_DAILY_LIMIT, GLOBAL_HOURLY_LIMIT, USER_DAILY_LIMIT, USER_HOURLY_LIMIT);
+
+var blocked = function(limits) {
+	// Add the request to retry scheduler or something
+	console.log("LIMIT REACHED! Type: " + limits);
+};
+
+var allowed = function(uid) {
+	// Let the request go through
+	console.log("REQUEST ALLOWED for user: " + uid);
+};
+
+rl.call('123456', allowed, blocked);
