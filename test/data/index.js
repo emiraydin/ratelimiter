0c47f2b06d0bebf9b003c3c2d1e14d06e88d9dad
Emir Aydin <emir@emiraydin.com>
2015-01-24 13:40:20 -0500

    Add more to the test data set

--- test/data/index.js
+++ test/data/index.js
@@ -1,10 +1,17 @@
 var Limit = require('../../ratelimiter/limit');
 
-var l1 = new Limit("{sourceName}:global:daily", 86400, 4000);
-var l2 = new Limit("{sourceName}:global:hourly", 3600, 2000);
-var l3 = new Limit("{sourceName}:{userID}:daily", 86400, 500);
-var l4 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
+// First limit set for multi limit tests
+var l1 = new Limit("{sourceName}:global:daily", 86400, 4000),
+	l2 = new Limit("{sourceName}:global:hourly", 3600, 2000),
+	l3 = new Limit("{sourceName}:{userID}:daily", 86400, 500),
+	l4 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
 
-var limits = [l1,l2,l3,l4];
+// Second limit set for expire tests
+var l5 = new Limit("{sourceName}:global:daily", 86400, 4000),
+	l6 = new Limit("{sourceName}:global:hourly", 3600, 2000),
+	l7 = new Limit("{sourceName}:{userID}:daily", 86400, 500),
+	l8 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
+
+var limits = [[l1,l2,l3,l4],[l5,l6,l7,l8]];
 
 module.exports = limits;
