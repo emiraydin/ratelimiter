6c81a22c520a0607499bbda2e160d4335050c2ad
Emir Aydin <emir@emiraydin.com>
2015-01-23 23:59:25 -0500

    Initialize limit instances properly

--- ratelimiter/limit.js
+++ ratelimiter/limit.js
@@ -4,9 +4,9 @@ var Limit = function(keyName, ttl, maxLimit) {
 	// {userID} inside the key name will be replaced by actual user ID
 	this.keyName = keyName || "undefinedKey";
 	// Time to live, in seconds
-	this.ttl = ttl;
+	this.ttl = ttl || Number.MAX_VALUE;
 	// Maximum count for this limit
-	this.maxLimit = maxLimit;
+	this.maxLimit = maxLimit || Number.MAX_VALUE;
 	
 };
 
