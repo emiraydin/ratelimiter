dde3f7f4b211ed4a4c2e7e6323586b520faaf381
Emir Aydin <emir@emiraydin.com>
2015-01-27 09:41:09 -0500

    Add some async requests to sample code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var RateLimiter = require('./lib/ratelimiter'),
 // l3 = daily limit of 500 calls per user
 // l4 = hourly limit of 100 calls per user
 var l1 = new Limit("{sourceName}:global:daily", 86400, 50000),
-	l2 = new Limit("{sourceName}:global:hourly", 3600, 10000),
+	l2 = new Limit("{sourceName}:global:hourly", 3600, 199),
 	l3 = new Limit("{sourceName}:{userID}:daily", 86400, 500),
 	l4 = new Limit("{sourceName}:{userID}:hourly", 3600, 100);
 
@@ -33,10 +33,58 @@ var allow = function(uid) {
 	console.log("REQUEST ALLOWED for user: " + uid);
 };
 
-// Send some requests to the rate limiter
-var NUMBER_OF_CALLS = 501;
+// // Send some requests to the rate limiter
+var NUMBER_OF_CALLS = 200;
 for (var i = 0; i < NUMBER_OF_CALLS; i++) {
 	// Send a request to the rate limiter with userID=123456
 	// with allow and block callbacks as defined above
 	rl.request('123456', allow, block);
 }
+
+// Send requests one after another for a single user
+var requestDispatcher = function(i) {
+	if (i < NUMBER_OF_CALLS) {
+		rl.request('123456', function(err, res) {
+			if (err)
+				console.log("REQUEST NOT ALLOWED for user: " +
+					err.uid + " | LIMIT(s) REACHED: " + err.reachedLimits);
+			else
+				console.log("REQUEST ALLOWED for user: " + res.uid);
+			// Send the next request
+			requestDispatcher(i+1);
+		});
+	}
+};
+
+// Send requests for two users one after another
+var simultaneousRequestDispatcher = function(i) {
+
+	if (i < NUMBER_OF_CALLS) {
+		// If i is even, send request as user1, send it as user2 otherwise
+		if (i % 2 == 0) {
+			rl.request('user1', function(err, res) {
+				if (err)
+					console.log("REQUEST #" + (i+1) + " IS NOT ALLOWED for user: " +
+						err.uid + " | LIMIT(s) REACHED: " + err.reachedLimits);
+				else
+					console.log("REQUEST #" + (i+1) + " IS ALLOWED for user: " + res.uid);
+			});
+		} else {
+			rl.request('user2', function(err, res) {
+				if (err)
+					console.log("REQUEST #" + (i+1) + " IS NOT ALLOWED for user: " +
+						err.uid + " | LIMIT(s) REACHED: " + err.reachedLimits);
+				else
+					console.log("REQUEST #"+ (i+1) + " IS ALLOWED for user: " + res.uid);				
+			});
+		}
+		// Send the next request
+		simultaneousRequestDispatcher(i+1);
+	}
+
+};
+
+// Start sending the requests
+requestDispatcher(0);
+
+// simultaneousRequestDispatcher(0);
